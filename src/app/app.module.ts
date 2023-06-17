5ba1a9b0fafbf5182a230da0ec7004cb4cd5332c
Sergio Marentes Jimenez <sergiomarentes4@gmail.com>
2023-06-17 15:32:17 -0600

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -37,6 +37,7 @@ import { VerificarComponent } from './components/verificar/verificar.component';
 import { environment } from 'src/environments/environment';
 import { ConfirmationResultService } from './services/confirmation-result.service';
 import * as firebase from 'firebase/app';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
   export const firebaseConfig = {
     apiKey: "AIzaSyCFzaeFz6xRt7mrPFdhflg3I17h38_d9P0",
@@ -84,6 +85,12 @@ import * as firebase from 'firebase/app';
     DividerModule,
     FormsModule,
     HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     ConfirmationResultService,
